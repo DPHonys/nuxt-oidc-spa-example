908a13a07c58449443f45e8aa74500f9df34c0b5
Daniel Honys <danielhonys@gmail.com>
2025-11-11 14:22:35 +0100

    Check if SSR disabled

--- modules/oidc-spa.ts
+++ modules/oidc-spa.ts
@@ -2,8 +2,15 @@ import { addVitePlugin, defineNuxtModule } from 'nuxt/kit'
 import { oidcSpa } from 'oidc-spa/vite-plugin'
 
 export default defineNuxtModule({
-  setup() {
-    // TODO check for ssr false
+  setup(_options, nuxt) {
+    // Check if SSR is disabled
+    if (nuxt.options.ssr !== false) {
+      throw new Error(
+        'oidc-spa module requires SSR to be disabled. Please set `ssr: false` in your nuxt.config.ts'
+      )
+    }
+
+    // Add vite plugin to only client side
     addVitePlugin(
       oidcSpa({
         freezeFetch: true,
